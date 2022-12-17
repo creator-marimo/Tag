52fd908bd13cb45283ee1b8f71f899569e826eeb
Creator_marimo <octoharu74@gmail.com>
2022-12-17 13:48:47 +0900

    Part.4 障害物の作成とUIの作成

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody rb;
     public float speed=5;
+    public GameObject panel;
     // Start is called before the first frame update
     void Start()
     {
-
+        panel.SetActive(false);
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
@@ -20,5 +23,13 @@ public class PlayerController : MonoBehaviour
         movez = Input.GetAxis("Vertical");
         rb.AddForce(movex*speed, 0, movez*speed);
         
+    }
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+
+        if (collisionInfo.gameObject.tag == "enemy") {
+            panel.SetActive(true);
+            Time.timeScale = 0;
+        }
     }
 }
